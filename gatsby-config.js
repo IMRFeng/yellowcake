8d72e29ab44bc91583457ed7c83f14abeeaede03
Zhantao Feng <heymrfeng@gmail.com>
2020-04-01 08:57:26 +1300

    updates.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ const postcssPresetEnv = require('postcss-preset-env')
 
 module.exports = {
   siteMetadata: {
-    title: 'Yellowcake',
-    siteUrl: 'https://yellowcake.netlify.com'
+    title: 'amazing-franklin-115182',
+    siteUrl: 'https://amazing-franklin-115182.netlify.com'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
